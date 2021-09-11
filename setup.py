4b5bcdbeb88793cc449599d62076d78eab6a02b0
Anurag Kumar <mailanu98@gmail.com>
2021-09-11 21:41:17 +0530

    Update setup.py
    Updated Classifiers

--- setup.py
+++ setup.py
@@ -43,6 +43,10 @@ setuptools.setup(
     package_dir = {'DeBERTa':'DeBERTa'},
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
